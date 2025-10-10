0efe8895834c03e33ce72126e1e262a4a8ce102d
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-10 14:06:16 +0900

    feat: Task G solved

---
+++ src/train.ts
@@ -0,0 +1,16 @@
+// G - Task
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+function getHighestIndex(arr: number[]): number {
+    let max = Math.max(...arr);
+    return arr.indexOf(max);
+}
+
+console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
