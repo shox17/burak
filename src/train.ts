4d96a4acf1bb8e97939e596f8b5c6d9e4afdcca2
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-12 01:36:45 +0900

    feat: Task H solved

--- src/train.ts
+++ src/train.ts
@@ -8,9 +8,24 @@
 // Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
 // Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
 
-function getHighestIndex(arr: number[]): number {
-    let max = Math.max(...arr);
-    return arr.indexOf(max);
+// function getHighestIndex(arr: number[]): number {
+//     let max = Math.max(...arr);
+//     return arr.indexOf(max);
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
+
+// H - Task
+
+// shunday function tuzing, 
+// u integerlardan iborat arrayni argument sifatida qabul qilib, 
+// faqat positive qiymatlarni olib string holatda return qilsin
+
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+function getPositive(arr: number[]): string {
+    let positiveArr = arr.filter(num => num > 0);
+    return positiveArr.join('');
 }
 
-console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
+console.log(getPositive([1, -4, 2])); // "12"
