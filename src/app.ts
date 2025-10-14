ff4ba57d7f4ab12c9b70007c2eff92f716bccb84
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-14 14:30:37 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import router from './router';
+import routerAdmin from './routerAdmin';
 
 /** 1. ENTRANCE **/
 const app = express();
@@ -15,6 +16,7 @@ app.set("view", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4. ROUTES **/
-app.use('/', router);
+app.use('/admin', routerAdmin);   // EJS
+app.use('/', router);             // REACT
 
 export default app;
--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,29 +1,8 @@
 import { Request, Response } from 'express';
 import { T } from '../libs/types/common';
 
+// REACT
+
 const memberController: T = {}
-memberController.goHome = (req: Request, res: Response) => {
-    try {
-        res.send('Home Page');
-    } catch (error) {
-        console.error("Error, goHome:", error);
-    }
-}
-
-memberController.getLogin = (req: Request, res: Response) => {
-    try {
-        res.send('Login Page');
-    } catch (error) {
-        console.error("Error, getLogin:", error);
-    }
-}
-
-memberController.getSignup = (req: Request, res: Response) => {
-    try {
-        res.send('Signup Page');
-    } catch (error) {
-        console.error("Error, getSignup:", error);
-    }
-}
 
 export default memberController;
