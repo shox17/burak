243323f4bf8034abf8ea6170569c2e0a28935024
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-15 11:15:34 +0900

    feat: develop member schema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,61 @@
+import mongoose, {Schema} from "mongoose";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
+
+// Member Model Definition
+const memberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default:MemberType.USER
+    },
+
+    memberStatus: {
+        type: String,
+        enum:MemberStatus,
+        default:MemberStatus.ACTIVE
+    },
+
+    memberNick: {
+        type: String,
+        index: { unique: true, sparse: true},
+        required: true,
+    },
+
+    memberPhone: {
+        type: String,
+        index: { unique: true, sparse: true},
+        required: true,
+    },
+
+    memberPassword:{
+        type:String,
+        select: false,
+        required: true,
+    },
+
+    memberAddress:{
+        type:String,
+    },
+
+    memberDesc:{
+        type:String,
+    },
+
+    memberImage:{
+        type:String,
+    },
+
+    memberPoints:{
+        type:String,
+        default:0,
+    },
+
+},
+   { timestamps:true} // createdAt, updatedAt
+);
+
+export default mongoose.model("Member", memberSchema);
+
+
+
+
