2ef704523f9c483718c20e9e6fa728eeb969f0da
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-20 20:19:20 +0900

    fix: updated

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("EXECUTED SERVER TS");
+import dotenv from 'dotenv';
+dotenv.config(); 
+import mongoose from 'mongoose';
+import app from './app';
+
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log("MongoDB connected successfully");
+        const PORT =  process.env.PORT ?? 3003;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
