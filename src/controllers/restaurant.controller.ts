cc5118b077990a85a5d992ae3a6901637a6dd299
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-25 14:41:09 +0900

    feat: locals variable integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
@@ -113,6 +113,23 @@ restaurantController.checkAuthSession = async (
   } catch (err) {
     console.log("Error, processLogin", err);
     res.send(err);
+  }
+};
+
+restaurantController.verifyRestaurant = (
+  req: AdminRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.session?.member?.memberType === MemberType.RESTAURANT) {
+    req.member = req.session.member;
+    next();
+  } else {
+    const message = Message.NOT_AUTHENTICATED;
+    res.send(`
+      <script> alert("${message}"); window.location.replace('/admin/login'); </script>`
+    );
+
   }
 };
 
