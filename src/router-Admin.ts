942005426e13d37b4aecc440281784b8cac09190
shox17 <159898318+shox17@users.noreply.github.com>
2025-10-26 12:43:18 +0900

    feat: upload files into server via multer

--- src/router-Admin.ts
+++ src/router-Admin.ts
@@ -1,26 +1,44 @@
-import express from 'express';
+import express from "express";
 const routerAdmin = express.Router();
-import restaurantController from './controllers/restaurant.controller';
-import productController from './controllers/product.controller';
+import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /** Restaurant */
-routerAdmin.get('/', restaurantController.goHome);
+routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
-    .get('/login', restaurantController.getLogin)
-    .post('/login', restaurantController.processLogin);
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
 
 routerAdmin
-    .get('/signup', restaurantController.getSignup)
-    .post('/signup', restaurantController.processSignup);
+  .get("/signup", restaurantController.getSignup)
+  .post(
+    "/signup",
+    makeUploader("members").single("memberImage"),
+    restaurantController.processSignup
+  );
 
-routerAdmin.get('/logout', restaurantController.logout);
+routerAdmin.get("/logout", restaurantController.logout);
 
-routerAdmin.get('/check-me', restaurantController.checkAuthSession);
+routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Product */
-routerAdmin.get("/product/all", restaurantController.verifyRestaurant, productController.getAllProducts);
-routerAdmin.post("/product/create", restaurantController.verifyRestaurant, productController.createNewProduct);
-routerAdmin.post("/product/:id", restaurantController.verifyRestaurant, productController.updateChosenProduct);
+routerAdmin.get(
+  "/product/all",
+  restaurantController.verifyRestaurant,
+  productController.getAllProducts
+);
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  makeUploader("products").array("productImages", 5),
+  productController.createNewProduct
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
+);
 
 /**  User */
 
