010e64415c421dd06f54cafb1d1da7084797316b
shox17 <159898318+shox17@users.noreply.github.com>
2025-11-04 14:30:00 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -43,14 +43,14 @@ productController.createNewProduct = async (
 
     await productService.createNewProduct(data);
     res.send(
-      `<script>alert('Product created successfully'); window.location.replace('admin/product/all')</script>`
+      `<script>alert('Product created successfully'); window.location.replace('/admin/product/all')</script>`
     );
   } catch (err) {
     console.log("Error,createNewProduct : ", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace('admin/product/all') </script>`
+      `<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`
     );
   }
 };
