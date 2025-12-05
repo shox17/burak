a9d21a7322ae012b7a745e1776593c2eb5308a95
shox17 <159898318+shox17@users.noreply.github.com>
2025-12-05 13:26:38 +0900

    feat: Task solved

--- src/train.ts
+++ src/train.ts
@@ -475,9 +475,31 @@
 
 // MASALAN: randomBetween(30, 50) return 45
 
-function randomBetween(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+// function randomBetween(min: number, max: number): number {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// // Tekshirish:
+// console.log(randomBetween(30, 50)); 
+
+// ================================================
+
+// TASK ZC
+
+// Selisy (°C) shkalasi bo'yicha raqam qabul qilib, uni
+// Ferenhayt (°F) shkalisaga o'zgaritib beradigan function yozing.
+
+// MASALAN: celsiusToFahrenheit(0) return 32;
+// MASALAN: celsiusToFahrenheit(10) return 50;
+
+// Yuqoridagi misolda, 0°C, 32°F'ga teng.
+// Yoki 10 gradus Selsiy, 50 Farenhaytga teng.
+
+// °C va °F => Tempraturani o'lchashda ishlatiladigan o'lchov birligi.
+
+function celsiusToFahrenheit(celsius: number): number {
+    return (celsius * 1.8) + 32;
 }
 
-// Tekshirish:
-console.log(randomBetween(30, 50)); 
+console.log(celsiusToFahrenheit(0));  // Output: 32
+console.log(celsiusToFahrenheit(10)); // Output: 50
